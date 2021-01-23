f8330b14b355204417fe50242c35cde452ed29ca
Garrett Cox <gcox5714@gmail.com>
2021-01-23 17:51:41 -0500

    homepage validates login and changes stuff

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,18 +1,41 @@
 import {Backend} from './backend.js';
 const navBar = document.querySelector('nav-bar');
 const loginButton = document.querySelector('nav-bar').shadowRoot.querySelector('#login-button');
+const registerButton = document.querySelector('nav-bar').shadowRoot.querySelector('#register-button');
+const footLogin = document.querySelector('footer-pro').shadowRoot.querySelector('#login-button');
+const footRegister = document.querySelector('footer-pro').shadowRoot.querySelector('#register-button');
+const headMessage = document.querySelector('#header-message');
+const headLogin = document.querySelector('#head-login-button');
+const headRegister = document.querySelector('#head-register-button');
 
 const API = new Backend();
 API.setBaseUrl('http://127.0.0.1:5000');
+
+let user = {};
 window.onload = () => {
-    //console.log(navBar);
-    //console.log(loginButton);
-    //console.log(loginButton.href);
     API.get('/login/v')
     .then(response => {
         if (response.session == 'true') {
+            user = response;
             loginButton.textContent= 'Profile';
-            loginButton.href = 'profile.html';
+            loginButton.setAttribute('href', 'profile.html');
+            registerButton.textContent = 'Logout';
+            registerButton.setAttribute('href', '#');
+            registerButton.addEventListener('click', () => {
+                API.get('/logout')
+                .then(response => {
+                    if (response.success == 'true') {
+                        location.reload();
+                    }
+                })
+            });
+            headMessage.innerHTML = `<em>Welcome, ${user.firstName}! Click the button below to reserve your spot at a ride or restaurant!<em>`;
+            headRegister.style.display = 'none';
+            headLogin.textContent = 'Reserve';
+            headLogin.setAttribute('href','reserve.html');
+            footRegister.style.display = 'none';
+            footLogin.textContent = 'Reserve';
+            footLogin.setAttribute('href','reserve.html');
         }
     })
 }
