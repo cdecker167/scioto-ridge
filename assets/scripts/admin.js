665414391629a412f005e4d50c63c2fcdf75f986
Garrett Cox <gcox5714@gmail.com>
2021-01-27 00:32:41 -0500

    admin and modify data done

--- assets/scripts/admin.js
+++ assets/scripts/admin.js
@@ -6,9 +6,11 @@ const selectedRes = document.querySelector('#selected-res');
 const waitTime = document.querySelector('#waittime');
 const open = document.querySelector('#open');
 const closed = document.querySelector('#closed');
+const button = document.querySelector('#submit-button');
 const table = document.querySelector('#report');
-const tableLand = document.querySelector('#table-landing');
+const form = document.querySelector('#modify-data');
 let attInfo = {};
+let isClosed = '';
 let FORMSELECTED = 0;
 
 const API = new Backend();
@@ -17,8 +19,8 @@ API.setBaseUrl('http://127.0.0.1:5000');
 window.onload = () => {
     API.get('/admin/v')
     .then(response => {
-        console.log(response);
         attInfo = response;
+        console.log(response);
     });
     API.get('/totalres')
     .then(response => {
@@ -28,14 +30,17 @@ window.onload = () => {
 
 attractions.forEach(att => {
     att.addEventListener('click', () => {
+        button.value = 'Submit';
         FORMSELECTED = att.dataset.idNum;
-        console.log(FORMSELECTED);
         table.innerHTML=`<tr><th style="border-top-left-radius: 15px;">Time</th><th style ='border-top-right-radius: 15px;'>Confirmation Code</th></tr>`;
         selectedAtt.textContent = attInfo[`index${att.dataset.idNum}`].name;
         waitTime.value = attInfo[`index${att.dataset.idNum}`].wait_time;
-        if (attInfo[`index${att.dataset.idNum}`].closed == 'false') {
+        if (attInfo[`index${att.dataset.idNum}`].closed == '!false') {
             open.checked = true;
             closed.checked = false;
+        } else if (attInfo[`index${att.dataset.idNum}`].closed == '!true') {
+            open.checked = false;
+            closed.checked = true;
         }
         API.get(`/getres/${att.dataset.idNum}`)
         .then(response => {
@@ -50,4 +55,30 @@ attractions.forEach(att => {
             selectedRes.textContent = `${i}`;
         });
     });
-})
+});
+
+form.addEventListener('click', () => {
+    if (waitTime.value && (open.checked == true || closed.checked == true) && FORMSELECTED != 0) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+    if (closed.checked) {
+        isClosed = '!true';
+    } else if (open.checked) {
+        isClosed = '!false';
+    }
+});
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    API.post('/change-attraction',{
+        attraction: `${FORMSELECTED}`,
+        time: `${waitTime.value}`,
+        isClosed: `${isClosed}`
+    })
+    .then(response => {
+        button.disabled = true;
+        button.value = 'Success';
+    })
+});
