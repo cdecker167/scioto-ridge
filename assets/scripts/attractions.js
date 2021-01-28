bc1094043bcf62ba3d51543d598c4dd8136954d5
Garrett Cox <gcox5714@gmail.com>
2021-01-27 19:15:08 -0500

    stars work in attractions

--- assets/scripts/attractions.js
+++ assets/scripts/attractions.js
@@ -1,6 +1,7 @@
 import {Backend} from './backend.js';
 const times = document.querySelectorAll('.time');
-const ratings = [];
+const stars = document.querySelectorAll('.starz');
+const ratings = [`<span style='color: #ef3e3d'></span><span style='color: #696969'>★★★★★</span>`,`<span style='color: #ef3e3d'>★</span><span style='color: #696969'>★★★★</span>`,`<span style='color: #ef3e3d'>★★</span><span style='color: #696969'>★★★</span>`,`<span style='color: #ef3e3d'>★★★</span><span style='color: #696969'>★★</span>`,`<span style='color: #ef3e3d'>★★★★</span><span style='color: #696969'>★</span>`,`<span style='color: #ef3e3d'>★★★★★</span><span style='color: #696969'></span>`];
 
 const API = new Backend();
 API.setBaseUrl('http://127.0.0.1:5000');
@@ -14,5 +15,13 @@ window.onload = () => {
             times[currentModal].textContent = `${response[`index${i+1}`].waitTime} minutes`;
             currentModal++;
         }
+    });
+    API.get('/stars')
+    .then(response => {
+        let currentStar = 0;
+        response.ratings.forEach(rate => {
+            stars[currentStar].innerHTML = `${ratings[Number.parseInt(rate.rating, 10)]}`;
+            currentStar++;
+        });
     });
 }
