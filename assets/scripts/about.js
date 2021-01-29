405c9353d5d2991482a661cde00b931b7a79f6b6
Garrett Cox <gcox5714@gmail.com>
2021-01-28 22:48:46 -0500

    final commit?

---
+++ assets/scripts/about.js
@@ -0,0 +1,34 @@
+import {Backend} from './backend.js';
+const loginButton = document.querySelector('nav-bar').shadowRoot.querySelector('#login-button');
+const registerButton = document.querySelector('nav-bar').shadowRoot.querySelector('#register-button');
+const footLogin = document.querySelector('footer-pro').shadowRoot.querySelector('#login-button');
+const footRegister = document.querySelector('footer-pro').shadowRoot.querySelector('#register-button');
+
+const API = new Backend();
+API.setBaseUrl('https://whispering-garden-35353.herokuapp.com');
+
+
+/*the /login/v route returns whether the user is logged in with an active session.
+If they are, the buttons change */
+window.onload = () => {
+    API.get('/login/v')
+    .then(response => {
+        if (response.session == 'true') {
+            loginButton.textContent = 'Profile';
+            loginButton.setAttribute('href','profile.html');
+            registerButton.textContent = 'Logout';
+            registerButton.setAttribute('href','#');
+            registerButton.addEventListener('click', () => {
+                API.get('/logout')
+                .then(response => {
+                    if (response.success == 'true') {
+                        window.location.href = 'index.html';
+                    }
+                })
+            });
+            footRegister.style.display = 'none';
+            footLogin.textContent = 'Reserve';
+            footLogin.setAttribute('href','reserve.html');
+        }
+    })    
+}
